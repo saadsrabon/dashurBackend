124d48f2b2e459666c43a802cb4240176255b219
Saad Srabon <saadsrabon2@gmail.com>
2024-02-08 00:16:31 +0600

    2024-02-08 00:16:31 - controller level added

---
+++ src/app/modules/users/resume.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import catchAsync from "../../../shared/catchAsync";
+import httpStatus from "http-status";
+import { ResumeService } from "./resume.service";
+import { sendSuccessResponse } from "../../../shared/customResponse";
+import { IResume } from "./resume.interface";
+
+const createResumeController = catchAsync(
+    async (req: Request, res: Response) => {
+        const { resume } = req.body;
+        const result = await ResumeService.createResume( resume);
+    
+        sendSuccessResponse<IResume>(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Resume created successfully!",
+        data: result,
+        });
+    }
+    );
